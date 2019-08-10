d1231c2150d3f664ade5e45f71d6010857a9e716
Ronald Stephens <rstephens@bluemorpho.cc>
2019-08-10 18:49:53 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,7 +8,7 @@ class Backer
 
   def back_project(title)
      @backed_projects << title
-    if @backed_projects << title
+    if @backed_projects.include?(title)
       Project.add_backer
     end
   end
